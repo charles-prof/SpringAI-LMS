3043085da50a9c0a23e3c0d8e7fe43050d646054
Charles Kumar <gamescharlie79@gmail.com>
2025-10-30 11:45:42 +0530

    create project using spring initializr
    * using java 21 for now
    * Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/springailms/games/LmsGameApplication.java
@@ -0,0 +1,13 @@
+package com.springailms.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springailms/games/LmsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springailms.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
