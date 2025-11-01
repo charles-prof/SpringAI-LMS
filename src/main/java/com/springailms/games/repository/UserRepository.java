2be1470f1533638c5e3a5421c51552c633791f45
Charles Kumar <gamescharlie79@gmail.com>
2025-11-01 14:13:18 +0530

    add CourseRepository and UserRepository interfaces for course and user data access

---
+++ src/main/java/com/springailms/games/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.springailms.games.repository;
+
+import com.springailms.games.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
